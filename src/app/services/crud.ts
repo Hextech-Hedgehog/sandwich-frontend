c2232e8e812a083241e3cf205cd9a7ae0331fc10
[Kim Wauters] <[solidarity.forever2@gmail.com]>
2022-12-16 14:51:41 +0100

    adds superclass CRUD

---
+++ src/app/services/crud.ts
@@ -0,0 +1,49 @@
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+export class CrudConfig {
+  single: (id) => string
+  many: string;
+}
+
+export abstract class Crud<T>
+{
+  protected http: HttpClient;
+  protected config: CrudConfig;
+
+  constructor(
+    httpClient: HttpClient,
+    config: CrudConfig
+  ) {
+    this.http = httpClient;
+    this.config = config;
+  }
+
+  findAll(): Observable<Array<T>>
+  {
+    // localhost/api/user
+    return this.http.get<Array<T>>(environment.api.url + this.config.many);
+  }
+
+  find(id: number)
+  {
+    // localhost/api/user/1
+    return this.http.get<T>(environment.api.url + this.config.single(id))
+  }
+
+  insert(entity: T)
+  {
+    return this.http.post<T>(environment.api.url + this.config.many, entity);
+  }
+
+  update(entity: T)
+  {
+    return this.http.put<T>(environment.api.url + this.config.many, entity);
+  }
+
+  delete(id: number)
+  {
+    return this.http.delete<T>(environment.api.url + this.config.single(id));
+  }
+}
